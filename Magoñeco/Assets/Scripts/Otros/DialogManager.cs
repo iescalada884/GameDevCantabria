e0a8a375b729b54a119ba3c7d647db878dc61523
iescalada884 <ied884@alumnos.unican.es>
2024-06-29 18:37:26 +0200

    Assests y dialogues better

--- Magoñeco/Assets/Scripts/Otros/DialogManager.cs
+++ Magoñeco/Assets/Scripts/Otros/DialogManager.cs
@@ -3,22 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEditor.ShaderGraph.Serialization;
+using System.IO;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public class Dialogue
+{
+    public string character;
+    public string text;
+}
+
+public class DialogueCointainer
+{
+    public Dialogue[] dialogues;
+}
 
 public class DialogManager : MonoBehaviour
 {
     //variables
+    public bool gano = true;
+
     public TextMeshProUGUI cuadroDialogo;
+    public TextMeshProUGUI personaje;
 
-    public string[] lines;
-
+    public int diaglogue;
     public float textSpeed = 0.1f;
-
+    DialogueCointainer dialogueContainer;
+    
+     Dialogue[] dialogues;
+   
     int index;
 
-    // Start is called before the first frame update
-    void Start()
+// Start is called before the first frame update
+void Start()
     {
+       LoadDialogues(diaglogue);
+
        StartDialogue();
+       personaje.text = "???";
     }
 
     // Update is called once per frame
@@ -30,14 +54,35 @@ public class DialogManager : MonoBehaviour
     public void StartDialogue()
     {
         index = 0;
-        Debug.Log("tets");
-        StartCoroutine(WriteLine());
+
+        if (!gano)
+            index = 1;
+       
     }
 
+    void LoadDialogues(int id)
+    {
+        string path = String.Format("dialogues/dialogues{0}", id);
+
+        TextAsset asset = Resources.Load<TextAsset>(path);
+
+        Debug.Log(asset);
+        string jsonTextFile = asset.text;
+
+        if (jsonTextFile != null)
+        {
+            dialogueContainer = JsonUtility.FromJson <DialogueCointainer> (jsonTextFile);
+            dialogues = dialogueContainer.dialogues;
+        }
+        else
+        {
+            Debug.LogError("No se encontró el archivo de diálogos");
+        }
+    }
 
     IEnumerator WriteLine()
     {
-        foreach (char letter in lines[index].ToCharArray()) {
+        foreach (char letter in dialogues[index].text.ToCharArray()) {
             cuadroDialogo.text += letter;
 
             yield return new WaitForSeconds(textSpeed);
@@ -48,12 +93,20 @@ public class DialogManager : MonoBehaviour
     public void NextLine()
     {
        StopAllCoroutines();
-        if (index < lines.Length - 1)
+        if (index < dialogues.Length)
         {
-            index++;
-
             cuadroDialogo.text = string.Empty;
+            personaje.text = dialogues[index].character;
             StartCoroutine (WriteLine());
+
+            if (index == 0)
+            {
+                index = 2;
+            }
+            else
+            {
+                index++;
+            }
         }
         else
         {
