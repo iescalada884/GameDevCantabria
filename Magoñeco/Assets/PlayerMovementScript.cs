24d831935600f339dd210d343176fb34c4d16355
mbmdevelop <mbm_dev@mailbox.org>
2024-06-29 18:35:55 +0100

    Labyrinth WIP

--- Magoñeco/Assets/PlayerMovementScript.cs
+++ Magoñeco/Assets/PlayerMovementScript.cs
@@ -10,8 +10,9 @@ public class PlayerMovementScript : MonoBehaviour
 
     public float MovementSpeed = 0.0f;
     public float JumpForce = 0.0f;
+    public bool VerticalMovementEnabled = false;
     public bool JumpLimiter = true;
-    private bool CanJump = true;
+    public bool CanJump = true;
     
     void Start()
     {
@@ -28,11 +29,23 @@ public class PlayerMovementScript : MonoBehaviour
         Vector3 direction = Vector2.zero;
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            direction = Vector3.right;
+            direction += Vector3.right;
         }
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            direction = Vector3.left;
+            direction += Vector3.left;
+        }
+
+        if (VerticalMovementEnabled)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            {
+                direction += Vector3.up;
+            }
+            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            {
+                direction += Vector3.down;
+            }
         }
 
         if (Input.GetKey(KeyCode.Space))
