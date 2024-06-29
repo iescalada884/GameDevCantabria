2ffbbfbd0e91ca3b38f3d9b679ae09eca0f3bd9d
mbmdevelop <mbm_dev@mailbox.org>
2024-06-29 12:53:22 +0100

    JumpMinigame(Player, Platforms, Spawner)

---
+++ Magoñeco/Assets/PlayerMovementScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody = null;
+
+    public float MovementSpeed = 0.0f;
+    public float JumpForce = 0.0f; 
+    private bool CanJump = true;    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
+        Vector3 direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            direction = Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            direction = Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        transform.position += direction * MovementSpeed * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (!CanJump)
+        {
+            return;
+        }
+
+        Rigidbody.velocity = Vector2.up * JumpForce;
+        CanJump = false;
+    }
+
+    void ResetJump()
+    {
+        CanJump = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject CollidedGameObject = collision.gameObject;
+        if (CollidedGameObject.tag == "Platform")
+        {
+            ResetJump();
+        }
+    }
+}
---
+++ Magoñeco/Assets/SpawnerBehaviourScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBehaviourScript : MonoBehaviour
+{
+    public GameObject SpawnObjectPrefab;
+    private GameObject[] SpawnPoints = new GameObject[3];
+    
+    public float SpawnCadence = 0.0f;
+    private float SpawnTimer = 0.0f;
+
+    void GetSpawnPoints()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            SpawnPoints[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    void Start()
+    {
+        GetSpawnPoints();
+    }
+
+    void Update()
+    {
+        SpawnTimer += Time.deltaTime;
+        if (SpawnTimer >= SpawnCadence)
+        {
+            SpawnObject();
+            SpawnTimer = 0.0f;
+        }
+    }
+
+    void SpawnObject()
+    {
+        int SpawnPointIndex = Random.Range(0, 3);
+        GameObject SpawnPoint = SpawnPoints[SpawnPointIndex];
+        Instantiate(SpawnObjectPrefab, SpawnPoint.transform.position, Quaternion.identity);
+    }
+}
